35311474b911687c03da814964d1f68184032e88
Sarazua <sesarazua@scl.edu.gt>
2025-07-24 09:10:18 -0600

    Se termina el primer diseño final de header

--- componentes/header/headerComponent.js
+++ componentes/header/headerComponent.js
@@ -10,9 +10,24 @@ export function header() {
     divTareas.className = "div-tareas";
     header.appendChild(divTareas);
 
+    let tituloTareas = document.createElement('h2');
+    tituloTareas.textContent = 'Pendientes';
+    divTareas.appendChild(tituloTareas);
+
+    let contadorTareas = document.createElement('div');
+    contadorTareas.className = "contador-tareas";
+    contadorTareas.textContent = '0';
+    divTareas.appendChild(contadorTareas);
+
     let divLogoUsuario = document.createElement('div');
     divLogoUsuario.className = "div-logo-usuario";
     header.appendChild(divLogoUsuario);
+
+    let imgUsuario = document.createElement('img');
+    imgUsuario.src = "https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1606/tuktukdesign160600119/59070200-icono-de-usuario-hombre-perfil-hombre-de-negocios-avatar-icono-persona-en-la-ilustraci%C3%B3n.jpg?ver=6";
+    imgUsuario.alt = "Logo Usuario";
+    imgUsuario.className = "logo-usuario";
+    divLogoUsuario.appendChild(imgUsuario);
 
     return header;
 }
